fdcfa1f6b67827dc9a09535e72fb969320b6dd6f
kyrielrvingcoding <kyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.com123456>
2016-04-18 17:24:25 +0800

    创建第一个文件

---
+++ cooking/cooking/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  cooking
+//
+//  Created by 诸超杰 on 16/4/18.
+//  Copyright © 2016年 诸超杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"诸超杰");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
